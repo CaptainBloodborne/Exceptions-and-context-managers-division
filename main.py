439cd8a799cfd5465271a1e3cd1afbee5b309918
Arina Tiukina <arina_tiukina@epam.com>
2021-03-18 12:36:47 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+from typing import Union
+
+
+def divide(str_with_ints: str) -> Union[float, str]:
+    """
+    Returns the result of dividing two numbers or an error message.
+    :arg
+        str_with_ints: str, ex. "4 2";
+    :return
+        result of dividing: float, ex. 2.0 (4 / 2 = 2.0);
+        error response in "Error code: {error message}: str;
+    """
+    raise NotImplementedError('Implement me!')
