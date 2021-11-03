ccea7ccf31c6d2cf7c599a4d1b414ab7ffc068a8
artem <vrail30@outlook.com>
2021-11-03 15:20:37 +0300

    Task done.

--- main.py
+++ main.py
@@ -10,4 +10,8 @@ def divide(str_with_ints: str) -> Union[float, str]:
         result of dividing: float, ex. 2.0 (4 / 2 = 2.0);
         error response in "Error code: {error message}: str;
     """
-    raise NotImplementedError('Implement me!')
+    a, b = str_with_ints.split(" ")
+    try:
+        return int(a) / int(b)
+    except Exception as error:
+        return f"Error code: {error}"
